7c422e10db750558165308e6a066a6c18c0026bb
Vishesh Singh <VisheshSingh16@gmail.com>
2020-12-29 19:45:00 -0600

    logout user

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,13 +4,27 @@
       <p>hey there... display name here</p>
       <p class="email">You are logged in as... email</p>
     </div>
-    <button>Logout</button>
+    <div class="error" v-if="error">{{ error }}</div>
+    <button @click="handleLogout">Logout</button>
   </nav>
 </template>
 
 <script>
+import useLogout from '../composables/useLogout';
+import { useRouter } from 'vue-router';
+
 export default {
   name: 'Navbar',
+  setup() {
+    const { error, logout } = useLogout();
+    const router = useRouter();
+
+    const handleLogout = async () => {
+      await logout();
+      router.push('/');
+    };
+    return { error, handleLogout };
+  },
 };
 </script>
 
